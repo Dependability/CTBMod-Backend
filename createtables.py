f0384fe63cf8c9e0bb12859ab1670f0aabb17e6e
Seyi Bakare <58313642+Dependability@users.noreply.github.com>
2024-08-02 21:27:51 -0500

    Update createtables.py

--- createtables.py
+++ createtables.py
@@ -1,7 +1,7 @@
 from app import app, db, ColdTurkeyPass
 
 with app.app_context():
-    db.create_table()
+    db.create_all()
     db.session.add(ColdTurkeyPass(computer="computer", locked=False, password="hi", confirm="unlocked"))
     db.session.add(ColdTurkeyPass(computer="computer", locked=False, password="hi", confirm="unlocked"))
     db.session.commit()
