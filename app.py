588703478a70fb3dad95f5135dd7df54f13ee2a7
Seyi Bakare <58313642+Dependability@users.noreply.github.com>
2024-08-02 20:45:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def unlock():
             emit("unlock", {"locked": False, "password": "test-password"}, json=True, to=computerType) #Use the past password]
     
 if __name__ == "__main__":
-    socketio.run(app,debug = True)
+    socketio.run(app)
